3b87d8d6dc314d8892c8eb56c9a9b44f10737855
Damião <damiao.jose@hotmail.com>
2024-08-02 23:16:07 -0300

    Correção swagger

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,32 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
+   /**
+ * @OA\Post(
+ *     path="/api/login",
+ *     summary="Login",
+ *     tags={"Login"},
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(
+ *             required={"email", "password"},
+ *             @OA\Property(property="email", type="string", format="email"),
+ *             @OA\Property(property="password", type="string", format="password")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="Login efetuado com sucesso",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="token", type="string", description="JWT token")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=401,
+ *         description="Credendiais inválidas"
+ *     ),
+ * )
+ */
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
@@ -18,6 +44,23 @@ class AuthController extends Controller
 
         return response()->json(compact('token'));
     }
+
+   /**
+ * @OA\Post(
+ *     path="/api/logout",
+ *     summary="Logout",
+ *     tags={"Login"},
+ *     security={{ "Bearer": {} }},
+ *     @OA\Response(
+ *         response=200,
+ *         description="Desconectado com sucesso"
+ *     ),
+ *     @OA\Response(
+ *         response=401,
+ *         description="Não autorizado"
+ *     ),
+ * )
+ */
 
     public function logout()
     {
